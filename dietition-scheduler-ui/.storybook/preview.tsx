e8d5002487dfe104f98b928ae97bcfa76600f9e8
Kyeongho <dagh1218@naver.com>
2026-09-01 23:39:51 +0900

    feat: #4 기본 css 디자인 변경
    - 화면 디자인 시안에 따른 전역 css 변경
    - 기본 레이아웃 정리

--- dietition-scheduler-ui/.storybook/preview.tsx
+++ dietition-scheduler-ui/.storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type {Preview} from '@storybook/nextjs-vite'
 import {mswLoader} from "msw-storybook-addon/csf3";
+import "../src/app/globals.css"
 
 const preview: Preview = {
     parameters: {
@@ -9,6 +10,9 @@ const preview: Preview = {
                 date: /Date$/i,
             },
         },
+        nextjs: {
+            appDirectory: true,
+        }
     },
     loaders: [mswLoader()]
 };
